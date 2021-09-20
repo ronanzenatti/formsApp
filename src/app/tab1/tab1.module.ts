0c9fc807e516ddf63170ab1d32388eb9b68bbed8
Ronan Adriel Zenatti <ronan.zenatti@etec.sp.gov.br>
2021-09-20 20:15:06 -0300

    a

---
+++ src/app/tab1/tab1.module.ts
@@ -0,0 +1,18 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Tab1Page } from './tab1.page';
+
+import { Tab1PageRoutingModule } from './tab1-routing.module';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule, ReactiveFormsModule,
+    Tab1PageRoutingModule
+  ],
+  declarations: [Tab1Page]
+})
+export class Tab1PageModule {}
